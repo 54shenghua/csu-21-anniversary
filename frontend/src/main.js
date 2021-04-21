54df306e2f9f4255ff119d421c1ed0f218ff6e5d
wangsz12 <1002590897@qq.com>
2021-04-21 23:54:43 +0800

    挂载接口

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { DatetimePicker, Popup, Toast, Loading } from 'vant'
+import apiConfig from './config/api'
 
 Vue.config.productionTip = false
 Vue.use(DatetimePicker).use(Popup).use(Toast).use(Loading)
+Vue.use(apiConfig)
 
 new Vue({
   router,
