4876543d995e3ab40371b3e3ce1875444c1faec1
wangsz12 <1002590897@qq.com>
2021-04-21 23:56:25 +0800

    完成加载样式

--- frontend/src/pages/Loading.vue
+++ frontend/src/pages/Loading.vue
@@ -1,22 +1,31 @@
 <template>
   <div class="container">
-
+      <van-loading size="24px" color="#fff" vertical>加载中</van-loading>
   </div>
 </template>
 
 <script>
+import { imgPreloader } from '../img-preloader'
+
 export default {
   name: 'Loading',
-  data () {
-    return {
-
-    }
+  mounted () {
+    (async () => {
+      await imgPreloader()
+      this.$router.replace({ name: 'home', params: { router: true } })
+    })()
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.container {
-  background-color: pink;
-}
+  .container {
+    background-image: url('../assets/bg.png');
+    background-position: bottom center;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: pink;
+  }
 </style>
