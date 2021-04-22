c6e52efc4ba335910d73b3927d6e58fe8bf033f1
wangsz12 <1002590897@qq.com>
2021-04-22 18:42:38 +0800

    完成过渡页面样式

--- frontend/src/pages/Transition.vue
+++ frontend/src/pages/Transition.vue
@@ -2,7 +2,13 @@
   <div class="container">
     <div class="logo" />
     <div class="content-box">
-
+      <div class="text-box">
+        <span>岳麓山下，湘江之滨</span>
+        <span>我们与中南相遇于此</span>
+        <span>那就在这儿</span>
+        <span>开启你的拾光之旅吧！</span>
+      </div>
+      <button @click="next">下一步</button>
     </div>
   </div>
 </template>
@@ -10,15 +16,17 @@
 <script>
 export default {
   name: 'Transition',
-  data () {
-    return {
-
+  methods: {
+    next () {
+      this.$router.replace({ name: 'profile', params: { router: true } })
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+  @import '../styles/global.scss';
+
   .container {
     background-image: url('../assets/2bg.png');
     background-position: center center;
@@ -45,6 +53,36 @@ export default {
       background-position: center center;
       background-size: cover;
       margin-top: 15%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+
+      .text-box {
+        margin-bottom: 15%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+
+        span {
+          display: block;
+          color: $font-color;
+          font-family: transition;
+
+          &:nth-child(1), &:nth-child(2) {
+            margin-bottom: 1.8rem;
+          }
+
+          &:nth-child(3), &:nth-child(4) {
+            font-size: 1.4rem;
+          }
+
+          &:nth-child(3) {
+            margin-bottom: 1.2rem;
+          }
+        }
+      }
     }
   }
 </style>
