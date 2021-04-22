25e772abd21713753ce68cde82283dc2e250914b
wangsz12 <1002590897@qq.com>
2021-04-22 18:42:28 +0800

    增加长按保存图片功能

--- frontend/src/pages/Summary.vue
+++ frontend/src/pages/Summary.vue
@@ -1,16 +1,48 @@
 <template>
-  <div class="container">
+  <div id="export" class="container">
 
   </div>
 </template>
 
 <script>
+import html2canvas from 'html2canvas'
+
 export default {
   name: 'Summary',
   data () {
     return {
 
     }
+  },
+  mounted () {
+    const content = document.getElementById('export')
+    const height = content.clientHeight
+    const width = content.clientWidth
+    const canvas = document.createElement('canvas')
+    const scale = 2
+
+    canvas.height = height
+    canvas.width = width
+    canvas.style.height = `${height * scale}px`
+    canvas.style.width = `${width * scale}px`
+    canvas.getContext('2d').scale(scale, scale)
+
+    const options = {
+      scale,
+      canvas,
+      logging: process.env.NODE_ENV === 'development',
+      height,
+      width,
+      useCORS: true
+    }
+
+    html2canvas(content, options)
+      .then((canvas) => {
+        const img = canvas.toDataURL('image/jpg')
+        const exportImg = document.createElement('img')
+        exportImg.src = img
+        document.body.appendChild(exportImg)
+      })
   }
 }
 </script>
