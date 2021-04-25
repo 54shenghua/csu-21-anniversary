495feb518d1e8e23414b72ec9ec4d8e109994824
Eric-lily <72089135+Eric-lily@users.noreply.github.com>
2021-04-25 19:11:43 +0800

    更改

--- BackEnd/api/views.py
+++ BackEnd/api/views.py
@@ -118,7 +118,7 @@ def get_ticket(request):
             jsapi_ticket = jsapi_ticket
             string1 = 'jsapi_ticket=' + jsapi_ticket + '&noncestr=' + var + '&timestamp=' + str(
                 now_time) + '&url=' + url
-            signature = hashlib.sha1(string1).hexdigest()
+            signature = hashlib.sha1(string1.encode('utf-8')).hexdigest()
             return JsonResponse(
                 {'data': {'noncestr': 'Wm3WZYTPz0wzccnW', 'timestamp': '1414587457', 'signature': signature},
                  'msg': '', 'status': 200})
