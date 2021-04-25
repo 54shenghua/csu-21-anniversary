b340e7b349a2e704e15b458b7489483ec9365286
wangsz12 <1002590897@qq.com>
2021-04-25 19:34:33 +0800

    调试

--- frontend/src/wxSDK.js
+++ frontend/src/wxSDK.js
@@ -12,7 +12,7 @@ export function initWXJSSDK (url) {
   Axios.post('ticket/', {
     url
   }).then((res) => {
-    alert(res.data.data)
+    alert(JSON.stringify(res.data.data))
     wx.config({
       debug: false,
       appId: 'wx2fdfc27744ffa252',
